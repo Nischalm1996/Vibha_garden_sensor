76b1f56d278d6adb44d4c47893779822a767d9ee
Nischal_M <nischal.m.edu@gmail.com>
2021-06-08 22:24:27 +0530

    repaired

--- 2.FIRMWARE/garden_Arduino/main/main.ino
+++ 2.FIRMWARE/garden_Arduino/main/main.ino
@@ -1,35 +1,27 @@
-
 #include "Arduino.h"
 #include "dht11.h"
 #include "sound.h"
 #include "soilMoisture.h"
-//#include "mpu.h"
+#include "mpu.h"
 #include "Button.h"
-#include <Wire.h>
-// The I2C LCD library
 #include <LiquidMenu.h>
-
 #include <LiquidCrystal_I2C.h>
-// The menu wrapper library
-// The I2C LCD object
-
-
+//push
 //OK  D5
 //LEFT  D7
 //RIGHT D4
 
 const bool pullup = false;
+
 Button left(7, pullup);
 Button right(4, pullup);
-//Button up(8, pullup);
-//Button down(9, pullup);
 Button enter(5, pullup);
 
 
 dhtClass temper;
 sound Noise;
 soilMoisture moisture;
-//mpuClass angle;
+mpuClass angle;
 
 struct allValues {
   float temp_cel;
@@ -42,6 +34,7 @@ struct allValues {
   int gyroz;
 };
 allValues toEsp = {temp, fahren, 0, 0, 0, 0, 0, 0};
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 LiquidLine welcome_line1(1, 0, "Environment");
 LiquidLine welcome_line2(1, 1, "Monitor System");
@@ -59,10 +52,8 @@ LiquidScreen screen3(gyrox, gyroy);
 LiquidMenu menu(lcd);
 
 void setup() {
-  // put your setup code here, to run once:
- // Serial.begin(9600);
-  //angle.begin();
-  temper.begin();
+  Serial.begin(9600);
+
   lcd.init();
   lcd.backlight();
   delay(500);
@@ -77,17 +68,17 @@ void setup() {
   menu.add_screen(Screen2);
   menu.add_screen(screen3);
   menu.update();
+  angle.begin();
+  temper.begin();
   delay(500);
   menu.next_screen();
-  menu.update();
+  //Serial.println("Setup done");
 }
-int count = 0;
+unsigned int count = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Check all the buttons
   if (right.check() == LOW)
   {
-   // Serial.println(F("RIGHT button pressed"));
+    // Serial.println(F("RIGHT button pressed"));
     menu.next_screen();
   }
   if (left.check() == LOW)
@@ -98,12 +89,9 @@ void loop() {
   if (enter.check() == LOW)
   {
     //Serial.println(F("ENTER button pressed"));
-    // Switches focus to the next line.
     menu.switch_focus();
   }
-
-
-  toEsp = {temp, fahren, humid, moisture.getRawReading(), Noise.getNoisePercentage(), 0, 0, 0};
+  toEsp = {temp, fahren, humid, moisture.getRawReading(), Noise.getNoisePercentage(), xaxis, yaxis, zaxis};
   //  Serial.print("celcius: "); Serial.print(toEsp.temp_cel); Serial.print(' ');
   //  Serial.print("Fahreheit: "); Serial.print(toEsp.temp_fah); Serial.print(' ');
   //  Serial.print("Humid: "); Serial.print(toEsp.humid); Serial.println("");
@@ -116,14 +104,14 @@ void loop() {
 
 
   count = count + 1;
-
+  //Serial.print(".");
   if (count >= 1000) {
     temper.updateTemp();
-    //angle.update//angles();
-    toEsp = {temp, fahren, humid, moisture.getRawReading(), Noise.getNoisePercentage(), 0, 0, 0};
-
+    angle.updateAngles();
+    toEsp = {temp, fahren, humid, moisture.getRawReading(), Noise.getNoisePercentage(), xaxis, yaxis, zaxis};
+    Serial.println("update");
     menu.update();
-    count=0;
+    count = 0;
   }
 
 }
--- 2.FIRMWARE/garden_Arduino/main/mpu.h
+++ 2.FIRMWARE/garden_Arduino/main/mpu.h
@@ -1,39 +1,32 @@
-//#ifndef _mpu_H
-//#define _mpu_H
-//#include "Arduino.h"
-//
-//#include <MPU6050_tockn.h>
-//#include <Wire.h>
-//MPU6050 mpu6050(Wire);
-//float xaxis = 0;
-//float yaxis = 0;
-//float zaxis = 0;
-//
-//
-//
-//class mpuClass
-//{
-//  public:
-//    void begin()
-//    {
-//      Wire.begin();
-//      mpu6050.begin();
-//      mpu6050.calcGyroOffsets(true);
-//
-//    }
-//    void updateAngles()
-//    {
-//      mpu6050.update();
-//      xaxis = mpu6050.getAngleX();
-//      yaxis = mpu6050.getAngleY();
-//      zaxis = mpu6050.getAngleZ();
-//    }
-//};
-//
-//
-//
-//
-//
-//
-//
-//#endif //_SOIL_MOISTURE_H
+#ifndef _mpu_H
+#define _mpu_H
+#include "Arduino.h"
+
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+MPU6050 mpu6050(Wire);
+float xaxis = 0;
+float yaxis = 0;
+float zaxis = 0;
+
+
+
+class mpuClass
+{
+  public:
+    void begin()
+    {
+      Wire.begin();
+      mpu6050.begin();
+      mpu6050.calcGyroOffsets(true);
+
+    }
+    void updateAngles()
+    {
+      mpu6050.update();
+      xaxis = mpu6050.getAngleX();
+      yaxis = mpu6050.getAngleY();
+      zaxis = mpu6050.getAngleZ();
+    }
+};
+#endif //_SOIL_MOISTURE_H
